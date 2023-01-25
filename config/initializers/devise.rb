213b981ce3dca6ee2377f588e63abba053ff12a9
日下陸矢 <rikuya.98@gmail.com>
2023-01-25 22:50:51 +0900

    ・userモデルの作成 ・photscontrollerの作成

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -95,8 +95,8 @@ Devise.setup do |config|
   # By default Devise will store the user in session. You can skip storage for
   # particular strategies by setting this option.
   # Notice that if you are skipping storage for all authentication paths, you
-  # may want to disable generating routes to Devise's sessions controller by
-  # passing skip: :sessions to `devise_for` in your config/routes.rb
+  # may want to disable generating routes to Devise's  controller by
+  # passing skip: : to `devise_for` in your config/routes.rb
   config.skip_session_storage = [:http_auth]
 
   # By default, Devise cleans up the CSRF token on authentication to
@@ -241,8 +241,8 @@ Devise.setup do |config|
   # config.encryptor = :sha512
 
   # ==> Scopes configuration
-  # Turn scoped views on. Before rendering "sessions/new", it will first check for
-  # "users/sessions/new". It's turned off by default because it's slower if you
+  # Turn scoped views on. Before rendering "/new", it will first check for
+  # "users//new". It's turned off by default because it's slower if you
   # are using only default views.
   # config.scoped_views = false
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2023_01_24_001611) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "account_name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -23,7 +24,7 @@ ActiveRecord::Schema.define(version: 2023_01_24_001611) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email", "account_name"], name: "index_users_on_email_and_account_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
