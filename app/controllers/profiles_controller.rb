e1688b91113d50a08a1f6b4220a07ea6f0997084
日下陸矢 <rikuya.98@gmail.com>
2023-01-29 16:36:18 +0900

    プロフィールページの作成 アクティブレコードの導入

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,32 @@
 class ProfilesController < ApplicationController
+ before_action :authenticate_user!
+
     def show
-        @profile = current_user.profile
+    @profile = current_user.profile
+    end
+
+    def edit
+    @profile = current_user.build_profile
+    end
+
+    def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+        redirect_to profile_path, notice: 'プロフィール更新'
+    else
+        flash.now[:error] = '更新できませんでした'
+        render :edit
+    end
+    end
+
+    private
+    def profile_params
+    params.require(:profile).permit(
+        :account_name,
+        :introduction,
+        :gender,
+        :subscribed,
+        :avatar
+        )
     end
 end
